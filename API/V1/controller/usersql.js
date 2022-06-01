b8c933dabe4d85e9fe8665b0eeadb33a1393c531
RAZ <raz62015@gmail.com>
2022-06-01 09:15:14 +0300

    user commit

--- API/V1/controller/usersql.js
+++ API/V1/controller/usersql.js
@@ -12,60 +12,70 @@ const config={
 module.exports={
 
     Login:(req,res)=>{
-        mssql.connect(config).then(function(conn){
-            const {username,password}=req.body;
-            const Sql=`SELECT * from T_users where Username='${username}'`;
+        mssql.connect(config).
+        then(function(conn){          
+            const {Username,Pass}=req.body;
+            const Sql=`SELECT * FROM T_users WHERE username='${Username}'`;           
             conn.query(Sql).then(function(rows,error){
-                if(error || rows.recordset.length!=1)
-                return res.status(409).json({msg:"user name or pass not found "});
-
+               if(error || rows.recordset.length!=1 )               
+                        return res.status(409).json({Msg:"Username or Password Not Found",Err:error});
                 const user=rows.recordset[0];
-                bcrypt.compare(password,user.password).then((status)=>{
-                    if(!status)
-                    return res.status(409).json({msg:"user name or pass not found "});
-                    const token= jwt.sign({username,Uid:user.Uid},process.env.SECRET_KEY,{expiresIn:'1H'});
-                    return res.status(200).json({Uid:user.Uid,token});
-                });
-            })
-        }).catch(function(err){
-            console.log(err);
-            return res.status(500).json({err});
+                bcrypt.compare(Pass,user.Pass).then((status)=>{
+                        if(!status)
+                            return res.status(409).json({Msg:"Username or Password Not Found"});
 
+                      
+                     //   const token=jwt.sign({Username:user.Username},process.env.SECRET_KEY,{expiresIn:'1H'});
+                        const token=jwt.sign({Username,_id:user._id},process.env.SECRET_KEY,{expiresIn:'1H'});
+                            return res.status(200).json({UID:user.UID,token:token});
+
+
+                }); 
+            
+            }).catch(function(err){
+                console.log(err);
+                return res.status(500).json(err);
+            });
+
+        })
+        .catch(function(err){
+            console.log(err);
+            return res.status(500).json(err);
         });
         },
         
-    Register:(req,res)=>{
-        mssql.connect(config).then(function(conn){
-            const {Username,password} = req.body;
-            const Sql = `Select * from T_users where Username='${Username}'`;
-          
-            conn.query(Sql).then(function(rows){ console.log(2);
-                if(rows.recordset.length != 0)
-                    return res.status(401).json({Err:"Username already Exist"});
-
-                    bcrypt.hash(password,12).then((hash)=>{
-                        console.log(hash);
-                        const Sql = `insert into T_users (Username,Pass) values ('${Username}','${hash}')`;
-                        console.log(Sql);
-                        conn.query(Sql).then((rows,error)=>{ console.log(3);                      
-                            return res.status(200).json({msg:"User Registered successfully"});   
-                                
-                        }).catch((err)=>{
-                          
-                            return res.status(500).json({err});
-                        });
-                    }).catch((err)=>{
-                       
-                        return res.status(500).json({err});
-                    });
-            }).catch((err)=>{
-                return res.status(500).json({err});
-            });
-        }).catch((err)=>{
-            return res.status(500).json({err});
-        });
-    }
-
-
-    
-}
+        Register:(req,res)=>{
+            console.log(config);
+             mssql.connect(config).then(function(conn){
+                 
+                 const {Username,Pass}=req.body; 
+                // Check If User Already Exist
+                 const Sql=`select * from T_users Where Username='${Username}'`;
+                 conn.query(Sql).then(function(rows){
+                  console.log("Check Exist");
+                     if(rows.recordset.length!=0)// If Exist- return 401 message
+                          return res.status(401).json({Msg:"Cant Register User Already Exist"});
+                     
+                      bcrypt.hash(Pass,12).then((hashPass)=>{               
+                          console.log("bcrypt");
+                          const Sql=`insert into T_users(Username,Pass) values('${Username}','${hashPass}')`; 
+                          conn.query(Sql).then(function(rows,error){
+                              if(error)
+                                  return res.status(500).json({Msg:`Error - Cant Register `,Err:error});
+                              // Return Success Msg
+                              return res.status(200).json(rows);
+              
+                              });
+                              
+                              }).catch(function(err){
+                                  console.log(err);
+                                  return res.status(500).json(err);
+                              });
+                      });   
+                   // Create Hash for Password and then Register User        
+                 }).catch(function(err){
+                  console.log(err);
+                  return res.status(500).json(err);
+              });            
+          }          
+};
